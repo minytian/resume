5aa008bb488bb93f8301d7a06719519230adacdf
Maxim <minytian@yandex.ru>
2019-09-01 22:51:51 +0300

    Сделал примерный вид макета. Сделал карточки в модальных окнах, а так же ссылки макетов на github.io Осталось поработать с фоном, сделать описание и мобильную вёрстку

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,18 +1,101 @@
 
-const open = document.querySelector('.works__modal')
-const myModal = document.querySelector('.myModal')
-const close = document.querySelector('.close')
+// модальные окна
+const open__biruang = document.querySelectorAll('.works__modal')[0]
+const open__pricing = document.querySelectorAll('.works__modal')[1]
+const open__design = document.querySelectorAll('.works__modal')[2]
+const open__atlantida = document.querySelectorAll('.works__modal')[3]
+const open__smitler = document.querySelectorAll('.works__modal')[4]
+const modal__biruang = document.querySelector('#modal__biruang')
+const modal__pricing = document.querySelector('#modal__pricing')
+const modal__design = document.querySelector('#modal__design')
+const modal__atlantida = document.querySelector('#modal__atlantida')
+const modal__smitler = document.querySelector('#modal__smitler')
+const close__biruang = document.querySelectorAll('.close')[0]
+const close__pricing = document.querySelectorAll('.close')[1]
+const close__design = document.querySelectorAll('.close')[2]
+const close__atlantida = document.querySelectorAll('.close')[3]
+const close__smitler = document.querySelectorAll('.close')[4]
 
-open.onclick = function () {
-	myModal.style.display = 'block'
+open__biruang.onclick = function () {
+		modal__biruang.style.display = 'block'
 }
 
-close.onclick = function () {
-	myModal.style.display = 'none'
+open__pricing.onclick = function () {
+		modal__pricing.style.display = 'block'
+}
+
+open__design.onclick = function () {
+		modal__design.style.display = 'block'
+}
+
+open__atlantida.onclick = function () {
+		modal__atlantida.style.display = 'block'
+}
+
+open__smitler.onclick = function () {
+		modal__smitler.style.display = 'block'
 }
 
 window.onclick = function (event) {
-	if (event.target == myModal) {
-		myModal.style.display = 'none'
+	if (event.target === modal__biruang || event.target === modal__pricing 
+		|| event.target === modal__design || event.target === modal__atlantida
+		|| event.target === modal__smitler ) {
+		modal__biruang.style.display = 'none'
+		modal__pricing.style.display = 'none'
+		modal__design.style.display = 'none'
+		modal__atlantida.style.display = 'none'
+		modal__smitler.style.display = 'none'
 	}
+}
+
+close__biruang.onclick = function () {
+	modal__biruang.style.display = 'none'
+
+}
+close__pricing.onclick = function (event) {
+	modal__pricing.style.display = 'none'
+}
+
+close__design.onclick = function (event) {
+	modal__design.style.display = 'none'
+}
+
+close__atlantida.onclick = function (event) {
+	modal__atlantida.style.display = 'none'
+}
+
+close__smitler.onclick = function (event) {
+	modal__smitler.style.display = 'none'
+}
+
+
+
+// Убираем placeholder при focuse
+// Кстати, можно было сделать просто в html-разметке <input type="text" class="text" value="Введите текст" onfocus="this.value=''" onblur="this.value='Введите текст'">
+
+const feedback__enterName = document.querySelector('.feedback__enterName')
+const feedback__enterEmail = document.querySelector('.feedback__enterEmail')
+const feedback__enterText = document.querySelector('.feedback__enterText')
+
+feedback__enterName.onfocus = function () {
+	this.setAttribute('placeholder', '')
+}
+
+feedback__enterName.onblur = function () {
+	this.setAttribute('placeholder', 'Введите имя')
+}
+
+feedback__enterEmail.onfocus = function () {
+	this.setAttribute('placeholder', '')
+}
+
+feedback__enterEmail.onblur = function () {
+	this.setAttribute('placeholder', 'Введите имя')
+}
+feedback__enterText.onfocus = function () {
+	this.setAttribute('placeholder', '')
+}
+
+feedback__enterText.onblur = function () {
+	this.setAttribute('placeholder', 'Введите имя')
 }
