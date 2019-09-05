25cd4ba7ddfb4d9ba16967480bd4438935db4368
minytian <minytian@yandex.ru>
2019-09-06 00:32:36 +0300

    Сделал форму отправки и настроил phpMailer. Теперь форма отправки работает + ajax, чтобы страница не перезагружалась. Так же сделал адаптив. Вроде без косяков, но пока пусть будет экспериментальная ветка.

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -88,4 +88,27 @@ feedback__enterEmail.onblur = function () {
 
 feedback__enterText.onblur = function () {
 	this.setAttribute('placeholder', 'Введите имя')
-}
+};
+
+// ajax
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form.feedback__form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "../mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn();
+			setTimeout(function() {
+				$(th).find('.success').removeClass('active').fadeOut();
+				th.trigger("reset");
+			}, 3000);
+		});
+		return false;
+	});
+
+});
+
